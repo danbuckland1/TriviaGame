b6560c41127b6a98f358b4dcabfb2b52eb6cc4c8
danbuckland1 <buckd409@gmail.com>
2018-06-16 22:37:22 -0400

    Writing pseudo-code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,35 @@
+
 //User clicks start button on opening of game
-//timer appears with first question
+var startBtn = document.getElementById('start');
+
+//start button disappears
+
+//timer appears counting down from 25 sec and first question appears
+var timer = 30
+
+//if user chooses correct answer: 
+//message correct answer alert
+
+//timer resets 25sec
+
+//next question appeears
+
+//if user chooses incorrect answer: 
+//message incorrect answer alert
+
+//the correct answer is shown,timer restarts 
+
+//next question appears
+
+//if timer reaches 0, correct answer shown
+
+//next question appears
+
+//timer resets 25sec
+
+//at end of game score of correct and incorrect answers appear
+
+//start over button resets THE GAME not RELOADS PAGE
+
+
 
